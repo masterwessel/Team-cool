29a06d78992db318f8297ba8967d7e832b6f242b
masterwessel <wesselvdmaarel@hotmail.com>
2015-03-16 15:40:03 +0100

    modified:   Supercoolprogramma.py

--- Supercoolprogramma.py
+++ Supercoolprogramma.py
@@ -1,7 +1,27 @@
 def main():
- #call van verschillende methodes
- #joris
-def leesBestand(bestandsnaam):
+    naamBestand = "sequence"
+    leesBestand (naamBestand)
+    
+def leesBestand(bestand):
+    lees = open (bestand, "r")
+    startLezen = False
+    gbSequentie = ""
+    atcgSequentie = ""
+    counter = 0
+    for regel in bestand:
+        if "ORIGIN" in regel:
+            startLezen = True
+
+        if startLezen == True:
+            counter += 1
+
+        if counter > 1:
+            gbSequentie = gbSequentie + regel
+
+    for letter in sequence:
+        if letter in "atcg":
+            atcgSequentie += letter
+        
  #retourneert de sequentie uit het bestand
  #jacco
 def bepaalGCpercentage (sequentie):
